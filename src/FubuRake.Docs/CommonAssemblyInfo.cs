89c4f492fa59f2c61468cbad87683a3144691a39
jeremydmiller <jeremydmiller@yahoo.com>
2013-05-13 10:42:47 -0500

    declaring success with the docs for now

---
+++ src/FubuRake.Docs/CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// SAMPLE: CommonAssemblyInfo
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyDescription("1.1.0.51709")]
+[assembly: AssemblyProduct("FubuCore")]
+[assembly: AssemblyCopyright("Copyright 2008-2013 Jeremy D. Miller, Josh Arnold, Joshua Flanagan, et al. All rights reserved.")]
+[assembly: AssemblyTrademark("83be3ac3fe927bacc2e81b1197ae5bc0508802f3")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.51709")]
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
+// ENDSAMPLE
--- src/FubuRake.Docs/rakefile.rb
+++ src/FubuRake.Docs/rakefile.rb
@@ -16,6 +16,18 @@ solution = FubuRake::Solution.new do |sln|
 end
 # ENDSAMPLE
 
+# SAMPLE: enable-ripple
+solution = FubuRake::Solution.new do |sln|
+	sln.ripple_enabled = true
+end
+# ENDSAMPLE
+
+# SAMPLE: enable-fubudocs
+solution = FubuRake::Solution.new do |sln|
+	sln.fubudocs_enabled = true
+end
+# ENDSAMPLE
+
 
 # SAMPLE: override-options
 load 'lib/fuburake.rb'
@@ -35,4 +47,82 @@ end
 end
 
 puts "The build number is #{@solution.options[:build_number]}"
+# ENDSAMPLE
+
+# SAMPLE: default-compile
+solution = FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/FubuRake.sln'
+	}
+end
+# ENDSAMPLE
+
+# SAMPLE: version
+	sln.assembly_info = {
+		:product_name => "FubuRake",
+		:copyright => 'Copyright Some Year by Some Guy'
+	}
+# ENDSAMPLE
+
+
+
+# SAMPLE: compile-step
+@solution = FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/Bottles.sln'
+	}
+				 
+	sln.compile_step :compile_console, 'src/Bottles.Console/Bottles.Console.csproj'
+	sln.compile_step :compile_bottle_project, 'bottles-staging/BottleProject.csproj'
+end
+# ENDSAMPLE
+
+# SAMPLE: precompile
+@solution = FubuRake::Solution.new do |sln|
+	sln.compile = {
+		:solutionfile => 'src/Bottles.sln'
+	}
+
+	sln.precompile = [:bottle_assembly_package]
+end
+
+desc "does the assembly bottling of the AssemblyPackage test project"
+task :bottle_assembly_package => [:compile_bottle_project] do
+  # do the bottling...
+end
+# ENDSAMPLE
+
+
+# SAMPLE: clean-task
+@solution = FubuRake::Solution.new do |sln|
+	sln.clean = ['results', 'archive']
+end
+# ENDSAMPLE
+
+
+
+# SAMPLE: explicit-tests
+@solution = FubuRake::Solution.new do |sln|
+	sln.options = {
+		:unit_test_projects => ['Bottles.Tests']
+	}
+end
+# ENDSAMPLE
+
+# SAMPLE: integration-test
+@solution = FubuRake::Solution.new do |sln|
+	sln.integration_test = ['Bottles.IntegrationTesting']
+end
+# ENDSAMPLE
+
+
+# SAMPLE: custom-tasks
+@solution = FubuRake::Solution.new do |sln|
+	sln.defaults = [:ilrepack, :integration_test]
+	sln.ci_steps = [:ilrepack, :archive_gem]
+end
+
+task :ilrepack do
+    #something here...
+end
 # ENDSAMPLE
