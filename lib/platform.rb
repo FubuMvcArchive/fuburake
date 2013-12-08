265bf1905a39ae8e2d449ec198f19985138743f6
Alistair Bush <alistair.bush@gmail.com>
2013-12-08 22:17:49 +1300

    current start method is wrong as we really need to be starting a terminal. Therefore both a open and start method available.

--- lib/platform.rb
+++ lib/platform.rb
@@ -13,12 +13,27 @@ module Platform
 		!RUBY_PLATFORM.match("darwin").nil?
 	end
 
-	def self.start(path)
+	def self.open(path)
 		command = "start #{path}"
 		if self.is_linux
 			command = "xdg-open #{path}"
 		elsif self.is_darwin
 			command = "open #{path}"
+		end
+		sh command
+	end
+
+	#http://src.chromium.org/chrome/trunk/deps/third_party/xdg-utils/scripts/xdg-terminal
+	#	#https://github.com/ssokolow/profile/blob/master/supplemental/xdg-terminal
+	#		#http://www.microsoft.com/resources/documentation/windows/xp/all/proddocs/en-us/start.mspx?mfr=true
+	#
+	def self.start(command)
+		if self.is_linux
+			command = "xterm -e '#{command}'"
+		elsif self.is_darmin
+			command = "Terminal -e '#{command}'"
+		else
+			command = "start '#{command}'"
 		end
 		sh command
 	end
